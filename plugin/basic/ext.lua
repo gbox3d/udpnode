174b42d222b464a2819c0f48c65dc525c01a2a02
gbox3d <gbox3d@gmail.com>
2019-05-02 14:44:54 +0900

    4번포트 설명 주석 수정

--- plugin/basic/ext.lua
+++ plugin/basic/ext.lua
@@ -4,7 +4,7 @@ gpio.mode(0,gpio.OUTPUT) -- onoff 만 가능
 gpio.write(0,0)
 
 
-gpio.mode(4,gpio.OUTPUT) --상태표시용 ,내장 led , 1 켜짐, 0 은 꺼짐
+gpio.mode(4,gpio.OUTPUT) --상태표시용 ,내장 led , 0 켜짐, 1 은 꺼짐
 gpio.write(4,1)
 
 
